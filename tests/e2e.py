7917af3c9b42b6a0e8ccfb93ea94a3de4f7567f9
AZ <alon.sandberg@gmail.com>
2024-01-01 17:17:58 +0200

    solve the net::ERR_CONNECTION_REFUSED error incorporate DNS resolution settings

--- tests/e2e.py
+++ tests/e2e.py
@@ -12,7 +12,7 @@ def test_scores_service(url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+        command_executor='http://selenium-standalone-chrome:4444/wd/hub',
         options=chrome_options
     )
 
