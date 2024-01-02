40c729ac4302c1bcf5b0f023a4d215290c06109b
AZ <alon.sandberg@gmail.com>
2024-01-02 11:58:43 +0200

    added logging to e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,12 +1,14 @@
 import traceback
 import sys
+import logging
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def test_scores_service(url):
     chrome_options = Options()
@@ -17,28 +19,32 @@ def test_scores_service(url):
     )
 
     try:
+        logging.info(f"Opening URL: {url}")
         driver.get(url)
+        logging.info("Waiting for the score element to be present on the page.")
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "score"))
         )
         score_html_element = driver.find_element(By.ID, "score")
         score = int(score_html_element.text)
+        logging.info(f"Score found: {score}")
         return 1 <= score <= 1000
     except Exception as e:
-        print(f"Error produced during test execution: {e}")
-        traceback.print_exc()
+        logging.error("Error produced during test execution", exc_info=True)
         return False
     finally:
+        logging.info("Quitting the driver.")
         driver.quit()
 
 
 def main_function():
     application_url = "http://localhost:5001/score"
+    logging.info("Starting the test.")
     if test_scores_service(application_url):
-        print("Test Passed Successfully!")
+        logging.info("Test Passed Successfully!")
         sys.exit(0)
     else:
-        print("Test Failed!")
+        logging.error("Test Failed!")
         sys.exit(-1)
 
 
